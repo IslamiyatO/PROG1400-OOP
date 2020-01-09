7db0d59e852118532a8f8034d6e7fa41a3317479
Islamiyat Odebunmi <w0438527@nscc.ca>
2020-01-09 12:17:54 -0400

    changed to display a number

--- HelloWorld/HelloWorld.java
+++ HelloWorld/HelloWorld.java
@@ -2,8 +2,13 @@
     class HelloWorld {
 
         public static void main(String[] args) {
+            // to create a new variable declare the variable nd name
+            int num;
+
+            num = 123465;
+
             // prints out hello
-            System.out.println("Hello World!");
+            System.out.println("num  = " + num);
         }
 
 
