48789246e6dc5db7d3f1880aeb4231921e132855
Islamiyat Odebunmi <w0438527@nscc.ca>
2020-01-16 12:28:01 -0400

    changed to display a number

--- Cookies/Cookies.java
+++ Cookies/Cookies.java
@@ -1,9 +1,12 @@
 package Cookies;
 
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
 public class Cookies {
     //A bag of cookies holds 40 cookies.
     //The calorie information on the bag claims
-    //that there are 10 servngs in the bag and that a serving equals 300 calories.
+    //that there are 10 servings in the bag and that a serving equals 300 calories.
     //Write a program that lets the user enter the
     //number of cookies he or she actually ate and
     //then reports the number of total calories consumed.
@@ -14,10 +17,28 @@ public class Cookies {
         int servingCalories = 300;
 
 
+
+        // make a new variable called scanner and use it to get input from the user
+        Scanner s = new Scanner(System.in);
+
+        System.out.print("Enter your name: ");
+        String name = s.nextLine();
+
         //    user enters number of cookies eaten
+        // to get the variable
+        System.out.print("How many cookies did you eat? ");
+        int numCookies = s.nextInt();
 
 
         //    output calories consumed
+        // create variable to hold number of servings
+        int servingSize = cookiesInABag / servingsInBag;
+        float userServings = (float)numCookies / servingSize;
+        //    calculate the number of calories
+        float userCalories =  userServings * servingCalories;
+
+        System.out.println(name + ", you ate " + userCalories + " calories.");
+
 
 
 
